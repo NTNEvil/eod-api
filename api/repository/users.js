0f8a42dc9e3b50c428e71d5b76b50f3844dc887f
Natan Marcos <natanmarcos11@gmail.com>
2023-02-23 12:41:07 -0300

    ADD inventory

--- api/repository/users.js
+++ api/repository/users.js
@@ -2,10 +2,10 @@ const supabase = require('./conn');
 
 async function login(user) {
     let { data: users, error } = await supabase
-    .from('users')
-    .select('id, username, created_at')
-    .like('username', user.username)
-    .like('password', user.password)
+        .from('users')
+        .select('id, username, created_at')
+        .like('username', user.username)
+        .like('password', user.password)
     if (users.length == 1) {
         return users[0];
     } else {
@@ -13,11 +13,11 @@ async function login(user) {
     }
 }
 
-async function getProfile(userId){
+async function getProfile(userId) {
     let { data: profiles, error } = await supabase
-    .from('profiles')
-    .select('*')
-    .eq('user_id', userId)
+        .from('profiles')
+        .select('*')
+        .eq('user_id', userId)
     if (profiles.length == 1) {
         return profiles[0];
     } else {
@@ -25,19 +25,35 @@ async function getProfile(userId){
     }
 }
 
-async function getProfiles(){
+async function getProfiles() {
     let { data: profiles, error } = await supabase
-    .from('profiles')
-    .select('*')
+        .from('profiles')
+        .select('*')
     return profiles;
 }
 
-async function getInventory(userId){
-    let { data: inventory, error } = await supabase
-    .from('inventory')
-    .select('*')
-    .eq('user_id', userId)
-    return inventory;
+async function getInventory(userId) {
+
+    let result = [];
+
+    let { data: inventory, error1 } = await supabase
+        .from('inventory')
+        .select('id, item_id')
+        .eq('user_id', userId)
+
+    for (let i = 0; i < inventory.length; i++) {
+        let { data:item, error2 } = await supabase
+        .from('items')
+        .select('*')
+        .eq('id', inventory[i].item_id)
+
+        let modelitem = {}
+        modelitem.id = inventory[i].id;
+        modelitem.name = item[0].name;
+        result.push(modelitem);
+    }
+
+    return result;
 }
 
 module.exports = {
