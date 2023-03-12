22d3a70c3ef035bd2af5edf48adb12ba99192131
Natan Marcos <natanmarcos11@gmail.com>
2023-03-12 10:19:03 -0300

    EDIT cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // cors
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://eod.vercel.app/");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
     next();
